c208bf14d8d5e8c1361fe21790f2c92c2dbff0d9
Abdelfattah1990 <abdew879@gmail.com>
2023-01-10 02:38:13 -0800

    task7

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,25 +1,15 @@
 #!/usr/bin/python3
-"""Program that adds elements to a list stored in a file.
-"""
+"""Add all arguments to a Python list and save them to a file."""
 import sys
 
-save_to_json_file = __import__("5-save_to_json_file").save_to_json_file
-load_from_json_file = __import__("6-load_from_json_file").load_from_json_file
+if __name__ == "__main__":
+    save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
+    load_from_json_file = \
+        __import__('6-load_from_json_file').load_from_json_file
 
-
-if __name__ == '__main__':
-    filename = "add_item.json"
-    # Load the data into the list object
     try:
-        data = load_from_json_file(filename)
-    except Exception:
-        data = []
-
-    # append each of the command line arguments to the list
-    for i in range(len(sys.argv)):
-        if i == 0:
-            continue
-        data.append(sys.argv[i])
-
-    # save the data back into the file
-    save_to_json_file(data, filename)
+        items = load_from_json_file("add_item.json")
+    except FileNotFoundError:
+        items = []
+    items.extend(sys.argv[1:])
+    save_to_json_file(items, "add_item.json")
